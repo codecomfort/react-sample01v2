95561c17d7ab189999445ca7039b5c9414518b59
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-05 01:46:59 +0900

    本文クリックでメニュー選択を解除

--- src/scripts/ui.ts
+++ src/scripts/ui.ts
@@ -1,33 +1,43 @@
 const onStart = (doc) => {
-    const $ = (element) => doc.getElementById(element);
+  const $ = (element) => doc.getElementById(element);
 
-    const toggleClass = (element, className) => {
-        const classes = element.className.split(/\s+/);
-        const len = classes.length;
+  const toggleClass = (element, className) => {
+    const classes = element.className.split(/\s+/);
+    const len = classes.length;
 
-        // マッチした className を除去
-        for (let ii = 0; ii < len; ii++) {
-            if (classes[ii] === className) {
-                classes.splice(ii, 1);  // 破壊的変更
-                break;
-            }
-        }
-        // className が見つからないならケツに付けておく
-        if (len === classes.length) {
-            classes.push(className);
-        }
+    // マッチした className を除去
+    for (let ii = 0; ii < len; ii++) {
+      if (classes[ii] === className) {
+        classes.splice(ii, 1);  // 破壊的変更
+        break;
+      }
+    }
+    // className が見つからないならケツに付けておく
+    if (len === classes.length) {
+      classes.push(className);
+    }
 
-        element.className = classes.join(' ');
-    };
+    element.className = classes.join(' ');
+  };
 
-    $('menuLink').onclick = (event) => {
-        event.preventDefault();
-        const active = 'active';
+  const toggleAll = (event) => {
+    event.preventDefault();
+    const active = 'active';
 
-        toggleClass($('layout'), active);
-        toggleClass($('menu'), active);
-        toggleClass($('menuLink'), active);
-    };
+    toggleClass($('layout'), active);
+    toggleClass($('menu'), active);
+    toggleClass($('menuLink'), active);
+  };
+
+  $('menuLink').onclick = (event) => {
+    toggleAll(event);
+  };
+
+  $('content').onclick = (event) => {
+    if ($('menu').className.indexOf('active') !== -1) {
+      toggleAll(event);
+    }
+  };
 };
 
-export default onStart
+export default onStart;
