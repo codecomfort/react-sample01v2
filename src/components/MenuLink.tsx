514caea6ba111a87721df7216a91896b46d76fbb
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-04-05 15:37:51 +0900

    MenuLinkのコメントを修正

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 
+// ブラウザ表示領域が小さくなった場合に出現させるハンバーガーアイコン
+// クリックで class プロパティに active をトグルで追加削除されるよう JS で制御
+// active の場合は left 値を変えてメニューを出現させる
 const MenuLink = () => (
   // Menu toggle
   <a href="#menu" id="menuLink" className="menu-link">
-    {/* Hamburger icon */}
     <span/>
   </a>
 );
